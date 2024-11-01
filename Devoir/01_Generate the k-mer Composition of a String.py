04f6d9ca9d3a9f1eaef158234070d8e3a8975e2e
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2024-11-01 19:20:33 +0100

    devoir 01

--- Devoir/01_Generate the k-mer Composition of a String.py
+++ Devoir/01_Generate the k-mer Composition of a String.py
@@ -4,6 +4,7 @@ def Compositionk(Text, k):
     k: An integer."""
     kmers = []  # create an empty list to store the k-mers
     for i in range(len(Text) - k + 1):  # iterate over the string
+        # +1 is added to len(Text) - k to include the last k-mer in the string
         kmers.append(Text[i : i + k])  # add the k-mer to the list
     return sorted(kmers)  # return the list of k-mers sorted in lexicographic order
 
