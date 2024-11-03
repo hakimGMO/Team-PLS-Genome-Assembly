424b52d72ee209828089113f8a0a5811b4d76297
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2024-11-03 14:03:52 +0100

    Refactorisation du code pour améliorer la lisibilité et la maintenabilité

--- 04_ De Bruijn graph/bruijn_test.py
+++ 04_ De Bruijn graph/bruijn_test.py
@@ -6,21 +6,26 @@ import matplotlib.pyplot as plt
 k = 4
 text = "AAGATTCTCTAC"
 
+
 def de_bruijn(k, text):
     # Créer le graphe dirigé
     G = nx.DiGraph()
     # Construire le graphe avec noeud source = prefixe et noeud destinataire = suffixe.
     # l'arete entre neoud source et destination, permet de ne pas creer les noeuds
     for i in range(len(text) - k + 1):
-        kmer = text[i:i + k]
+        kmer = text[i : i + k]
         prefix = kmer[:-1]
         suffix = kmer[1:]
-        G.add_edge(prefix, suffix) # declarer une arete permet de creer les noeuds automatiquement
+        G.add_edge(
+            prefix, suffix
+        )  # declarer une arete permet de creer les noeuds automatiquement
 
     # affichage liste d'adjacence
     for node in sorted(G.nodes()):
-        successors = sorted(G.neighbors(node)) # on recup les neouds suivants, par ordre alpha
+        successors = sorted(
+            G.neighbors(node)
+        )  # on recup les neouds suivants, par ordre alpha
         print(f"{node} -> {','.join(successors)}")
 
-de_bruijn(k, text)
 
+de_bruijn(k, text)
